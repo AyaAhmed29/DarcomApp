9f40ad914ecfb1391c3af17a1adbce10f772f138
Aya Ahmed <220210253@fa-hists.edu.eg>
2025-10-14 21:48:47 +0300

    App validator

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:darcom_app/core/logic/cubit/cubit/toggle_theme_cubit.dart';
+import 'package:darcom_app/core/logic/language/toggle_language_cubit.dart';
+import 'package:darcom_app/core/utils/app_route.dart';
+import 'package:darcom_app/core/utils/app_theme.dart';
+import 'package:darcom_app/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => ToggleLanguageCubit()),
+          BlocProvider(create: (context) => ToggleThemeCubit()),
+        ],
+        child: BlocBuilder<ToggleThemeCubit, ThemeData>(
+          builder: (context, theme) {
+            return BlocBuilder<ToggleLanguageCubit, Locale>(
+              builder: (context, state) {
+                return MaterialApp.router(
+                  localizationsDelegates: [
+                    S.delegate,
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                  ],
+                  locale: state,
+                  supportedLocales: S.delegate.supportedLocales,
+
+                  routerConfig: AppRoute.router,
+                  debugShowCheckedModeBanner: false,
+                  // theme: theme,
+                  theme: AppTheme.lightTheme,
+                  darkTheme: AppTheme.darkTheme,
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
