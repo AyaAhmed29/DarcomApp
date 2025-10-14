72d7e328d270b43dcf3eb3892a56057c27da9c5b
Aya Ahmed <220210253@fa-hists.edu.eg>
2025-10-14 21:50:17 +0300

    app language

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,186 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "AlreadyHaveAnAccount":
+            MessageLookupByLibrary.simpleMessage("Already have an account?"),
+        "BacktoLogin": MessageLookupByLibrary.simpleMessage("Back to Login"),
+        "Category": MessageLookupByLibrary.simpleMessage("Category "),
+        "Categorys": MessageLookupByLibrary.simpleMessage("Categories"),
+        "DontHaveAnAccount":
+            MessageLookupByLibrary.simpleMessage("Don\'t have an account?"),
+        "FeaturedProducts":
+            MessageLookupByLibrary.simpleMessage("Featured Products"),
+        "Filter": MessageLookupByLibrary.simpleMessage("Filter"),
+        "ForgotPasswordMassage": MessageLookupByLibrary.simpleMessage(
+            "Enter your email and we will send you a reset link"),
+        "Joinus":
+            MessageLookupByLibrary.simpleMessage("Join the Darcom family"),
+        "NewestFirst": MessageLookupByLibrary.simpleMessage("Newest First"),
+        "OldestFirst": MessageLookupByLibrary.simpleMessage("Oldest First"),
+        "PriceHighToLow":
+            MessageLookupByLibrary.simpleMessage("Price High to Low"),
+        "PriceLowToHigh":
+            MessageLookupByLibrary.simpleMessage("Price Low to High"),
+        "Recentsearches":
+            MessageLookupByLibrary.simpleMessage("Recent searches"),
+        "Result": MessageLookupByLibrary.simpleMessage("Result"),
+        "SearchForItems":
+            MessageLookupByLibrary.simpleMessage("Search for items..."),
+        "SearchResults": MessageLookupByLibrary.simpleMessage("Search Results"),
+        "SeeAll": MessageLookupByLibrary.simpleMessage("See All"),
+        "SortBy": MessageLookupByLibrary.simpleMessage("Sort By"),
+        "Theme": MessageLookupByLibrary.simpleMessage("Theme"),
+        "active": MessageLookupByLibrary.simpleMessage("Active"),
+        "activeOrders": MessageLookupByLibrary.simpleMessage("Active Orders"),
+        "addToCart": MessageLookupByLibrary.simpleMessage("Add To Cart"),
+        "address": MessageLookupByLibrary.simpleMessage("Address"),
+        "allFeatured": MessageLookupByLibrary.simpleMessage("All Featured"),
+        "ar": MessageLookupByLibrary.simpleMessage("AR"),
+        "authenticText": MessageLookupByLibrary.simpleMessage(
+            "You want Authentic, here you go!"),
+        "beauty": MessageLookupByLibrary.simpleMessage("Beauty"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+        "cancelConfirmationQuestion": MessageLookupByLibrary.simpleMessage(
+            "Are you sure you want to Cancel this order?"),
+        "canceled": MessageLookupByLibrary.simpleMessage("Canceled"),
+        "canceledOrders":
+            MessageLookupByLibrary.simpleMessage("Canceled Orders"),
+        "cart": MessageLookupByLibrary.simpleMessage("Cart"),
+        "checkout": MessageLookupByLibrary.simpleMessage("Checkout"),
+        "chooseProducts":
+            MessageLookupByLibrary.simpleMessage("Choose Products"),
+        "completed": MessageLookupByLibrary.simpleMessage("Completed"),
+        "completedOrders":
+            MessageLookupByLibrary.simpleMessage("Completed Orders"),
+        "confirmPassword":
+            MessageLookupByLibrary.simpleMessage("Confirm Password"),
+        "confirmation": MessageLookupByLibrary.simpleMessage("Confirmation"),
+        "createAccount":
+            MessageLookupByLibrary.simpleMessage("Create an account!"),
+        "createAccountButton":
+            MessageLookupByLibrary.simpleMessage("Create Account"),
+        "dark": MessageLookupByLibrary.simpleMessage("Dark"),
+        "dataUpdatedSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Data updated successfully"),
+        "deliveryAddress":
+            MessageLookupByLibrary.simpleMessage("Delivery Address"),
+        "deliveryFee": MessageLookupByLibrary.simpleMessage("Delivery Fee"),
+        "easyPayment": MessageLookupByLibrary.simpleMessage("Easy Payment"),
+        "email": MessageLookupByLibrary.simpleMessage("Email"),
+        "en": MessageLookupByLibrary.simpleMessage("EN"),
+        "error": MessageLookupByLibrary.simpleMessage("Error: "),
+        "fastDelivery": MessageLookupByLibrary.simpleMessage("Fast Delivery"),
+        "findItHere":
+            MessageLookupByLibrary.simpleMessage("Find it here, buy it now!"),
+        "findItHereBuyNow":
+            MessageLookupByLibrary.simpleMessage("Find it here, buy it now!"),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("Forgot Password?"),
+        "fullName": MessageLookupByLibrary.simpleMessage("Full Name"),
+        "getStarted": MessageLookupByLibrary.simpleMessage("Get Started"),
+        "home": MessageLookupByLibrary.simpleMessage("Home"),
+        "items": MessageLookupByLibrary.simpleMessage("Items"),
+        "language": MessageLookupByLibrary.simpleMessage("Language"),
+        "light": MessageLookupByLibrary.simpleMessage("Light"),
+        "logInToYourExistingAccount": MessageLookupByLibrary.simpleMessage(
+            "Log in to your existing account"),
+        "logOut": MessageLookupByLibrary.simpleMessage("Log Out"),
+        "logOutConfirmation": MessageLookupByLibrary.simpleMessage(
+            "Are you sure you want to log out from your account?"),
+        "logOutSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Log out successfully"),
+        "login": MessageLookupByLibrary.simpleMessage("Login"),
+        "loginSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Login successfully"),
+        "loremDescription1": MessageLookupByLibrary.simpleMessage(
+            "Browse a wide range of modern and classic furniture with high quality"),
+        "loremDescription2": MessageLookupByLibrary.simpleMessage(
+            "Exclusive offers and discounts on all types of furniture"),
+        "loremDescription3": MessageLookupByLibrary.simpleMessage(
+            "Fast delivery to your door with cash on delivery option"),
+        "loremTitle1": MessageLookupByLibrary.simpleMessage(
+            "Discover the best furniture for your home"),
+        "loremTitle2":
+            MessageLookupByLibrary.simpleMessage("Prices to fit every budget"),
+        "loremTitle3":
+            MessageLookupByLibrary.simpleMessage("Order and receive anytime"),
+        "myFavorites": MessageLookupByLibrary.simpleMessage("My Favorites"),
+        "myOrders": MessageLookupByLibrary.simpleMessage("My Orders"),
+        "myProfile": MessageLookupByLibrary.simpleMessage("My Profile"),
+        "next": MessageLookupByLibrary.simpleMessage("Next"),
+        "no": MessageLookupByLibrary.simpleMessage("No"),
+        "noItemsPrefix":
+            MessageLookupByLibrary.simpleMessage("You don\'t have any"),
+        "noItemsSuffix": MessageLookupByLibrary.simpleMessage("at this time"),
+        "order": MessageLookupByLibrary.simpleMessage("Order"),
+        "orderCanceled": MessageLookupByLibrary.simpleMessage("Order Canceled"),
+        "orderCanceledSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Order canceled successfully"),
+        "orderDelivered":
+            MessageLookupByLibrary.simpleMessage("Order delivered"),
+        "orderItem": MessageLookupByLibrary.simpleMessage("Order Item"),
+        "orderPlacedSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Order placed successfully"),
+        "orderTotal": MessageLookupByLibrary.simpleMessage("Order Total"),
+        "orders": MessageLookupByLibrary.simpleMessage("Orders"),
+        "password": MessageLookupByLibrary.simpleMessage("Password"),
+        "phone": MessageLookupByLibrary.simpleMessage("Phone"),
+        "placeOrder": MessageLookupByLibrary.simpleMessage("Place Order"),
+        "prev": MessageLookupByLibrary.simpleMessage("Prev"),
+        "product": MessageLookupByLibrary.simpleMessage("Product"),
+        "productId": MessageLookupByLibrary.simpleMessage("Product ID"),
+        "products": MessageLookupByLibrary.simpleMessage("Products"),
+        "profile": MessageLookupByLibrary.simpleMessage("Profile"),
+        "quantity": MessageLookupByLibrary.simpleMessage("Quantity"),
+        "recommended": MessageLookupByLibrary.simpleMessage("Recommended"),
+        "register": MessageLookupByLibrary.simpleMessage("Register"),
+        "registerAgreement": MessageLookupByLibrary.simpleMessage(
+            "By clicking the \"Register\" button, you agree \nto the public offer"),
+        "registerSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Register successfully"),
+        "save": MessageLookupByLibrary.simpleMessage("Save"),
+        "search": MessageLookupByLibrary.simpleMessage("Search"),
+        "searchAnyProduct":
+            MessageLookupByLibrary.simpleMessage("Search any Product.."),
+        "sendresetLink":
+            MessageLookupByLibrary.simpleMessage("Send reset link"),
+        "settings": MessageLookupByLibrary.simpleMessage("Settings"),
+        "shopNow": MessageLookupByLibrary.simpleMessage("Shop Now"),
+        "shoppingList": MessageLookupByLibrary.simpleMessage("Shopping List"),
+        "signInToContinue":
+            MessageLookupByLibrary.simpleMessage("Sign in to continue"),
+        "skip": MessageLookupByLibrary.simpleMessage("Skip"),
+        "subtotal": MessageLookupByLibrary.simpleMessage("Subtotal"),
+        "taxAndFees": MessageLookupByLibrary.simpleMessage("Tax and Fees"),
+        "total": MessageLookupByLibrary.simpleMessage("Total"),
+        "totalOrder": MessageLookupByLibrary.simpleMessage("Total Order"),
+        "trackDriver": MessageLookupByLibrary.simpleMessage("Track Driver"),
+        "typeAddress": MessageLookupByLibrary.simpleMessage(
+            "Type address here or pick from map"),
+        "welcomeBack": MessageLookupByLibrary.simpleMessage("Welcome Back!"),
+        "yes": MessageLookupByLibrary.simpleMessage("Yes"),
+        "youWantAuthentic": MessageLookupByLibrary.simpleMessage(
+            "You want Authentic, here you go!")
+      };
+}
