8216f72f9035367ac7b2bd5be15537ecdaef84d9
Aya Ahmed <220210253@fa-hists.edu.eg>
2025-10-14 21:55:16 +0300

    App image

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,165 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppIcon
+  /// assets/images/AppIcon.png
+  static const String imagesAppIcon = "assets/images/AppIcon.png";
+
+  /// Assets for imagesBedroomFurniture
+  /// assets/images/bedroom-furniture.jpg
+  static const String imagesBedroomFurniture = "assets/images/bedroom-furniture.jpg";
+
+  /// Assets for imagesDiningFurniture
+  /// assets/images/dining-furniture.jpg
+  static const String imagesDiningFurniture = "assets/images/dining-furniture.jpg";
+
+  /// Assets for imagesGetStart
+  /// assets/images/getStart.jpg
+  static const String imagesGetStart = "assets/images/getStart.jpg";
+
+  /// Assets for imagesGetStarted
+  /// assets/images/GetStarted.png
+  static const String imagesGetStarted = "assets/images/GetStarted.png";
+
+  /// Assets for imagesHeroFurniture
+  /// assets/images/hero-furniture.jpg
+  static const String imagesHeroFurniture = "assets/images/hero-furniture.jpg";
+
+  /// Assets for imagesIconsBag
+  /// assets/images/icons/Bag.png
+  static const String imagesIconsBag = "assets/images/icons/Bag.png";
+
+  /// Assets for imagesIconsBagBlack
+  /// assets/images/icons/BagBlack.png
+  static const String imagesIconsBagBlack = "assets/images/icons/BagBlack.png";
+
+  /// Assets for imagesIconsBagProfile
+  /// assets/images/icons/BagProfile.png
+  static const String imagesIconsBagProfile = "assets/images/icons/BagProfile.png";
+
+  /// Assets for imagesIconsCloseEye
+  /// assets/images/icons/closeEye.svg
+  static const String imagesIconsCloseEye = "assets/images/icons/closeEye.svg";
+
+  /// Assets for imagesIconsDecrease
+  /// assets/images/icons/decrease.png
+  static const String imagesIconsDecrease = "assets/images/icons/decrease.png";
+
+  /// Assets for imagesIconsEmail
+  /// assets/images/icons/Email.svg
+  static const String imagesIconsEmail = "assets/images/icons/Email.svg";
+
+  /// Assets for imagesIconsEye
+  /// assets/images/icons/eye.svg
+  static const String imagesIconsEye = "assets/images/icons/eye.svg";
+
+  /// Assets for imagesIconsFalse
+  /// assets/images/icons/false.svg
+  static const String imagesIconsFalse = "assets/images/icons/false.svg";
+
+  /// Assets for imagesIconsGroup
+  /// assets/images/icons/Group.png
+  static const String imagesIconsGroup = "assets/images/icons/Group.png";
+
+  /// Assets for imagesIconsHeart
+  /// assets/images/icons/Heart.png
+  static const String imagesIconsHeart = "assets/images/icons/Heart.png";
+
+  /// Assets for imagesIconsHeartIcon
+  /// assets/images/icons/HeartIcon.png
+  static const String imagesIconsHeartIcon = "assets/images/icons/HeartIcon.png";
+
+  /// Assets for imagesIconsHome
+  /// assets/images/icons/home.png
+  static const String imagesIconsHome = "assets/images/icons/home.png";
+
+  /// Assets for imagesIconsIncrease
+  /// assets/images/icons/increase.png
+  static const String imagesIconsIncrease = "assets/images/icons/increase.png";
+
+  /// Assets for imagesIconsLocation
+  /// assets/images/icons/location.png
+  static const String imagesIconsLocation = "assets/images/icons/location.png";
+
+  /// Assets for imagesIconsLocationSVG
+  /// assets/images/icons/locationSVG.svg
+  static const String imagesIconsLocationSVG = "assets/images/icons/locationSVG.svg";
+
+  /// Assets for imagesIconsLock
+  /// assets/images/icons/lock.svg
+  static const String imagesIconsLock = "assets/images/icons/lock.svg";
+
+  /// Assets for imagesIconsLogout
+  /// assets/images/icons/Logout.png
+  static const String imagesIconsLogout = "assets/images/icons/Logout.png";
+
+  /// Assets for imagesIconsLogoutPro
+  /// assets/images/icons/LogoutPro.png
+  static const String imagesIconsLogoutPro = "assets/images/icons/LogoutPro.png";
+
+  /// Assets for imagesIconsPhone
+  /// assets/images/icons/phone.svg
+  static const String imagesIconsPhone = "assets/images/icons/phone.svg";
+
+  /// Assets for imagesIconsProfile
+  /// assets/images/icons/Profile.png
+  static const String imagesIconsProfile = "assets/images/icons/Profile.png";
+
+  /// Assets for imagesIconsRedHard
+  /// assets/images/icons/redHard.png
+  static const String imagesIconsRedHard = "assets/images/icons/redHard.png";
+
+  /// Assets for imagesIconsSearch
+  /// assets/images/icons/search.png
+  static const String imagesIconsSearch = "assets/images/icons/search.png";
+
+  /// Assets for imagesIconsSetting
+  /// assets/images/icons/Setting.png
+  static const String imagesIconsSetting = "assets/images/icons/Setting.png";
+
+  /// Assets for imagesIconsShopping
+  /// assets/images/icons/shopping.png
+  static const String imagesIconsShopping = "assets/images/icons/shopping.png";
+
+  /// Assets for imagesIconsTrue
+  /// assets/images/icons/true.svg
+  static const String imagesIconsTrue = "assets/images/icons/true.svg";
+
+  /// Assets for imagesIconsUser
+  /// assets/images/icons/User.svg
+  static const String imagesIconsUser = "assets/images/icons/User.svg";
+
+  /// Assets for imagesIconsWhiteHard
+  /// assets/images/icons/whiteHard.png
+  static const String imagesIconsWhiteHard = "assets/images/icons/whiteHard.png";
+
+  /// Assets for imagesNoProdect
+  /// assets/images/NoProdect.png
+  static const String imagesNoProdect = "assets/images/NoProdect.png";
+
+  /// Assets for imagesOnbording1000
+  /// assets/images/onbording1000.png
+  static const String imagesOnbording1000 = "assets/images/onbording1000.png";
+
+  /// Assets for imagesOnbording20
+  /// assets/images/onbording20.png
+  static const String imagesOnbording20 = "assets/images/onbording20.png";
+
+  /// Assets for imagesOnbording5
+  /// assets/images/onbording5.png
+  static const String imagesOnbording5 = "assets/images/onbording5.png";
+
+  /// Assets for imagesPageViewItem1Image 
+  /// assets/images/PageViewItem1Image .svg
+  static const String imagesPageViewItem1Image  = "assets/images/PageViewItem1Image .svg";
+
+  /// Assets for imagesPageViewItem2Image 
+  /// assets/images/PageViewItem2Image .svg
+  static const String imagesPageViewItem2Image  = "assets/images/PageViewItem2Image .svg";
+
+  /// Assets for imagesPageViewItem3Image 
+  /// assets/images/PageViewItem3Image .svg
+  static const String imagesPageViewItem3Image  = "assets/images/PageViewItem3Image .svg";
+}
+
